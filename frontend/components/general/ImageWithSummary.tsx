76001fba55edc578cf5d534c83e082a80c9496f9
Linn <linn.jagestedt@gmail.com>
2023-02-08 12:07:13 +0100

    Updatera komponent

--- frontend/components/general/ImageWithSummary.tsx
+++ frontend/components/general/ImageWithSummary.tsx
@@ -17,19 +17,17 @@ export default function ImageWithSummary(props: InputProps) {
       direction={props.direction}
       justifyContent="center"
       alignItems="center"
-      sx={{paddingBottom: "80px", maxWidth: "1200px", margin: "auto"}}
+      sx={{ padding: "20px 0" }}
     >
-        <Grid item xs={7} md={5}>
-            <Image style={{ borderRadius: "40px"}} src={props.imageSrc} />
+        <Grid item xs={7} md={4}>
+            <Image style={{ borderRadius: "20px"}} src={props.imageSrc} />
         </Grid>
-        <Grid item xs={0} md={1}/>
+        <Grid item xs={7} md={1}/>
         <Grid item xs={9} md={5}>
             <Stack>
               <Typography variant="h2" align="center" sx={{ color: "inherit" }} >{props.title}</Typography>
-              <Typography variant="body1" align="center" sx={{ color: "inherit", fontSize: "19px"}}>{props.text}</Typography>
-              {props.link != "" &&
-                <Button href={props.link} variant="outlined" size="large" sx={{ margin: "40px auto" }}>Läs Mer</Button>
-              } 
+              <Typography variant="body1" align="center" sx={{ color: "inherit", fontSize: "24px"}}>{props.text}</Typography>
+              <Button href={props.link} variant="contained" size="large" sx={{ margin: "20px auto", backgroundColor: "#EC6610", color: "inherit" }}>Läs Mer</Button>
             </Stack>
         </Grid>
     </Grid>
