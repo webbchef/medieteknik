709d33f593fe81cf55fa0bc9872227db3fb99967
Berkay2002 <Berkayorhan@hotmail.se>
2023-04-14 11:02:23 +0200

    Made Instagram image carousel mobile/iPad compatible

--- components/studentliv/Studentkonto.tsx
+++ components/studentliv/Studentkonto.tsx
@@ -1,65 +1,87 @@
-import { Grid, Typography } from "@mui/material";
+import React, { useContext } from "react";
+import { Grid, Typography, Box } from "@mui/material";
 import WavyBackground from "../general/WavyBackground";
 import StyledButton from "../general/StyledButton";
 import ImageCarousel from "./ImageCarousel";
 import useInstagramPosts from "./useInstagramPosts";
+import { MobileStateContext } from "../../contexts/MobileContexts";
 
 export default function Studentkonto() {
   const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN ?? "";
   const posts = useInstagramPosts(accessToken);
+  const { isMobile, isIpad } = useContext(MobileStateContext);
 
-  return (
+  const paddingTopValue = () => {
+    if (isMobile) return "20px !important";
+    if (isIpad) return "30px !important";
+    return "70px !important";
+  };
+
+  const contentWidth = () => {
+    if (isMobile || isIpad) return "80%";
+    return "100%";
+  };
+
+  const contentMargin = () => {
+    if (isMobile || isIpad) return "0 auto";
+    return "unset";
+  };
+
+return (
     <WavyBackground bgColor="#13283c" textColor="#FFFFFF">
-      <Grid container spacing={3} maxWidth="lg">
-        <Grid
-          item
-          xs={12}
-          md={accessToken != "" ? 6 : 12}
-          sx={{
-            display: "flex",
-            paddingTop: "70px !important",
-            paddingBottom: "70px",
-            minHeight: "380px",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-          }}
-        >
-          <Typography variant="h2" sx={{ m: 2, color: "white" }}>
-            Studentkonto
-          </Typography>
-          <Typography sx={{ m: 2, color: "white" }}>
-            Vill du få en inblick i hur det är att leva som en MT-student eller
-            hur studentlivet är här i Norrköping?
-            <br />
-            <br />
-            Då ska du följa vårat instagramkonto <i>medieteknik_student</i> där
-            du varje vecka får du följa med en MT-student som delar med sig av
-            sin vardag!
-          </Typography>
-          <StyledButton
-            external={true}
-            src="https://www.instagram.com/medieteknik_student/?hl=en"
-          >
-            Till kontot!
-          </StyledButton>
-        </Grid>
-        {accessToken != "" && (
+      <Box sx={{ width: contentWidth(), margin: contentMargin() }}>
+        <Grid container spacing={3} maxWidth="lg">
           <Grid
             item
             xs={12}
-            md={6}
+            md={accessToken !== "" ? 6 : 12}
             sx={{
               display: "flex",
-              justifyContent: "center",
+              paddingTop: paddingTopValue(),
+              paddingBottom: "70px",
+              minHeight: "380px",
+              flexDirection: "column",
               alignItems: "center",
+              justifyContent: "center",
+              textAlign: "center",
             }}
           >
-            {posts && <ImageCarousel posts={posts} />}
+            <Typography variant="h2" sx={{ m: 2, color: "white" }}>
+              Studentkonto
+            </Typography>
+            <Typography sx={{ m: 2, color: "white" }}>
+              Vill du få en inblick i hur det är att leva som en MT-student eller
+              hur studentlivet är här i Norrköping?
+              <br />
+              <br />
+              Då ska du följa vårat instagramkonto <i>medieteknik_student</i> där
+              du varje vecka får du följa med en MT-student som delar med sig av
+              sin vardag!
+            </Typography>
+            <StyledButton
+              external={true}
+              src="https://www.instagram.com/medieteknik_student/?hl=en"
+            >
+              Till kontot!
+            </StyledButton>
           </Grid>
-        )}
-      </Grid>
+          {accessToken !== "" && (
+            <Grid
+              item
+              xs={12}
+              md={6}
+              sx={{
+                paddingTop: paddingTopValue(),
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {posts && <ImageCarousel />}
+            </Grid>
+          )}
+        </Grid>
+      </Box>
     </WavyBackground>
   );
 }
